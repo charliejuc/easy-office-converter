dc372651bfddb3f71987ddf2e924e83ecd273ec9
charliejuc <charlieinsous@gmail.com>
2016-08-28 23:50:48 +0200

    Refactoring code, doesn't pass always the tests yet. I need add remove .tmp files created by unoconv.

--- index.js
+++ index.js
@@ -5,78 +5,225 @@ var path = require('path')
 var util = require('util')
 var fs = require('fs')
 
-function officeConverter (filePath, options) {
-	self = {}
-	self.options = options || {}
-	self.filePath = filePath
+function getNumber (str) {
+	return Number(str.replace(/\D+/g, ''))
+}
 
-	self.allowQueue = self.options.allowQueue || false
-	self.timeout = self.options.timeout || 500
+function processExists (baseGrepCommand, cb) {
+	var cmd = baseGrepCommand + ' | wc -l'
 
-	if ( ! self.filePath ) throw new Error('filePath is required')	
-	
-	self.inputExt = path.extname(self.filePath)
-	self.inputFilename = path.basename(self.filePath, self.inputExt)
-	self.inputDirname = path.dirname(self.filePath)
-	
-	self.outputFilename = self.inputFilename
+	exec(cmd, (err, stdout, stderr) => {
+		if (err) return cb(err)
 
-	self.inputFileExists = function (callback) {
-		self.fileExists(self.filePath, callback)
+		var p_numb = getNumber(stdout)
+		var process_exists = ! isNaN(p_numb) && p_numb > 0
+
+		cb(null, process_exists, p_numb)
+	})
+}
+
+function listener (options) {
+	var listener = {}
+	listener.baseGrep = 'ps -aux | tr -s " " | grep -i "%s" | grep -v grep'
+	listener.unoconvListenerProcessName = 'unoconv -l'
+	listener.sofficeProcessName = 'soffice.bin'
+	listener.createCbCalled = false
+	listener.options = options || {}
+	listener.options.createTimeout = listener.options.createTimeout || 20000
+	listener.options.maxCreateTries = listener.options.maxCreateTries || 4
+	listener.options.createTriesTimeout = listener.options.createTriesTimeout || 250
+
+	listener._getListenerCommand = function () {
+		return listener._getProcessCommand(listener.unoconvListenerProcessName)
 	}
 
-	self.outputFileExists = function (callback) {
-		self.fileExists(self.getOutputFilePath(), callback)
+	listener._getSofficeCommand = function () {
+		return listener._getProcessCommand(listener.sofficeProcessName)
+	}
+
+	listener._getProcessCommand = function (process) {
+		var cmd = util.format(listener.baseGrep, process)
+		return cmd
+	}
+
+	listener.exists = function (cb) {
+		processExists(listener._getListenerCommand(), cb)
+	}
+
+	listener.sofficeExists = function (cb) {
+		processExists(listener._getSofficeCommand(), cb)
+	}
+
+	listener.pid = function (cb) {
+		var cmd = listener._getListenerCommand() + ' | cut -d " " -f 2'
+
+		exec(cmd, (err, stdout, stderr) => {
+			if (err) return cb(err)
+
+			var outSplit = stdout.split(/\n/)
+			
+			if (outSplit.length > 2) stdout = outSplit[0]			
+
+			var pid = getNumber(stdout)
+
+			cb(null, pid)
+		})
+	}
+
+	listener.kill = function (callback) {	
+		var execSofficeKill = true
+
+		function sofficeExists (cb) {
+			listener.sofficeExists((err, exists) => {
+				if (err) return cb(err)
+				if (! exists) execSofficeKill = false
+	
+				cb()
+			})
+		}
+
+		function execKillListener (err, pid, cb) {
+			if (err) return cb(err)
+			if (isNaN(pid) || pid === 0) return cb(new Error('Process do not exist'))	
+			var cmd = 'kill %s'
+			cmd = util.format(cmd, pid)		
+	
+			exec(cmd, err => {
+				if (err) return cb(err)
+				execKillSoffice(cb)
+			})
+		}
+	
+		function execKillSoffice (cb) {
+			if ( ! execSofficeKill ) return cb()
+			var cmd = 'killall -I soffice.bin'
+	
+			exec(cmd, cb)
+		}
+		
+		function killProcesses(cb) {
+			listener.pid((err, pid) => { 
+				execKillListener(err, pid, cb)
+			})
+		}
+	
+		async.series([
+			sofficeExists,
+			killProcesses	
+		], err => {
+			if (err) return callback(err)
+
+			callback()
+		})
+	}
+
+	listener.create = function (cb, createTries) {	
+		var cbWrapper = function (cb) {
+			return (err, stdout, stderr) => { 
+				if (listener.createCbCalled) return 
+
+				listener.createCbCalled = true
+				cb(err, stdout, stderr)
+			}
+		}(cb)
+		var cmd = listener.unoconvListenerProcessName
+		var endInit = setTimeout(cbWrapper, listener.options.createTimeout)
+		
+		createTries = createTries || 0
+		
+		function callbackCall (endInit, cbWrapper) {
+			var cbWrapperEnd = (err, stdout, stderr) => {
+				cbWrapper(err, stdout, stderr)
+
+				listener.createCbCalled = false
+			}
+
+			return (err, stdout, stderr) => {
+				clearTimeout(endInit)
+				if (err) return cbWrapperEnd(err, null, stderr)
+
+				function listenerExists (err, exists, p_numb) {
+					if (err) return cbWrapperEnd(err)
+					if (! exists && createTries < listener.options.maxCreateTries) { 
+						return setTimeout(() => listener.create(cb, ++createTries), listener.options.createTriesTimeout)
+					}
+					if (! exists) return cbWrapperEnd(new Error('Unoconv listener not running.'))
+
+					cbWrapper()
+				}
+
+				if (! listener.createCbCalled) return listener.exists(listenerExists)
+
+				cbWrapperEnd(err, stdout, stderr)
+			}
+		}
+
+		exec(cmd, callbackCall(endInit, cbWrapper))
+	}
+
+	return listener
+}
+
+function converter (filePath, options) {
+	var converter = {}
+
+	converter.options = options || {}
+	converter.filePath = filePath
+	converter.timeout = converter.options.timeout || 500
+
+	if ( ! converter.filePath ) throw new Error('filePath is required')	
+	
+	converter.inputExt = path.extname(converter.filePath)
+	converter.inputFilename = path.basename(converter.filePath, converter.inputExt)
+	converter.inputDirname = path.dirname(converter.filePath)
+	
+	converter.outputFilename = converter.inputFilename
+
+	converter.inputFileExists = function (callback) {
+		converter.fileExists(converter.filePath, callback)
+	}
+
+	converter.outputFileExists = function (callback) {
+		converter.fileExists(converter.getOutputFilePath(), callback)
 	}
 
 	
-	self.finalOutputFileExists = function (callback) {
-		self.fileExists(self.getFinalOutputFilePath(), callback)
+	converter.finalOutputFileExists = function (callback) {
+		converter.fileExists(converter.getFinalOutputFilePath(), callback)
 	}
 
 
-	self.fileExists = function (filePath, callback) {
+	converter.fileExists = function (filePath, callback) {
 		fs.access(filePath, fs.F_OK, callback)
 	}
 
-	self.addUniqueidToOutputFilename = function () {
-		self.outputFilename += util.format('_%s', uniqid())
+	converter.addUniqueidToOutputFilename = function () {
+		converter.outputFilename += util.format('_%s', uniqid())
 	}
 
-	self.setOutputOptions = function (outputOptions) {
-		if ( ! self.outputOptions || ( outputOptions && Object.keys(outputOptions).length )) {
-			self.outputOptions = outputOptions
+	converter.setOutputOptions = function (outputOptions) {
+		if ( ! converter.outputOptions || ( outputOptions && Object.keys(outputOptions).length )) {
+			converter.outputOptions = outputOptions
 		}
-
-		return self.outputOptions
 	}
 
-	self.appendToQueue = function () {
-		throw new Error("Code not finished")
-	}
+	converter.convert = function (callback, outputOptions) {
+		converter.setOutputOptions(outputOptions)
 
-	self.existsPreviousProcess = function () {
-		return undefined
-	}
-
-	self.convert = function (callback, outputOptions) {
-		self.setOutputOptions(outputOptions)
-
-		if ( ! self.outputOptions.outputFormat ) throw new Error('outputFormat option is required')
-		if ( self.existsPreviousProcess() ) return self.addToQueue(callback, outputOptions)
+		if ( ! converter.outputOptions || ! converter.outputOptions.outputFormat ) throw new Error('outputFormat option is required')	
 
 		function finalOutputFileExists (cb) {
 			var callback = function (err) {
-				if ( ! err) self.addUniqueidToOutputFilename()
+				if ( ! err) converter.addUniqueidToOutputFilename()
 				
 				cb()
 			}
 
-			self.finalOutputFileExists(callback)
+			converter.finalOutputFileExists(callback)
 		}
 
 		function convertProcess (cb) {
-			var convertCommand = self.getConvertCommand()
+			var convertCommand = converter.getConvertCommand()
 
 			exec(convertCommand, (err, stdout, stderr) => {
 				if (err) return cb(err)
@@ -86,7 +233,7 @@ function officeConverter (filePath, options) {
 		} 
 
 		function outputFileExists (cb) {
-			self.outputFileExists(err => {
+			converter.outputFileExists(err => {
 				if (err) return cb(err)
 
 				cb()
@@ -94,7 +241,7 @@ function officeConverter (filePath, options) {
 		}
 
 		function moveFile (cb) {
-			self.moveFileToOutput(err => {
+			converter.moveFileToOutput(err => {
 				if (err) return cb(err)
 
 				cb()
@@ -109,40 +256,43 @@ function officeConverter (filePath, options) {
 		], err => {
 			if (err) return callback(err)
 			
-			callback(null, self.getFinalOutputFilePath())
+			callback(null, converter.getFinalOutputFilePath())
 		})
 	}
-	
-	self.getFinalOutputFilePath = function () {
-		var oldPath = self.getOutputFilePath()
-		if ( ! self.outputOptions.outputDir ) return oldPath
+
+	converter.getFinalOutputFilePath = function () {
+		var oldPath = converter.getOutputFilePath()
+		if ( ! converter.outputOptions.outputDir ) return oldPath
 		
-		return path.join(self.outputOptions.outputDir, self.outputFilename + '.' + self.outputOptions.outputFormat)	
+		return path.join(converter.outputOptions.outputDir, converter.outputFilename + '.' + converter.outputOptions.outputFormat)	
 	} 
 
-	self.getOutputFilePath = function () {
-		self.outputFilePath = path.join(self.inputDirname, self.inputFilename + '.' + self.outputOptions.outputFormat )
+	converter.getOutputFilePath = function () {
+		converter.outputFilePath = path.join(converter.inputDirname, converter.inputFilename + '.' + converter.outputOptions.outputFormat )
 		
-		return self.outputFilePath
+		return converter.outputFilePath
 	}
 
-	self.moveFileToOutput = function (callback) {
-		if ( ! self.outputOptions.outputDir ) return callback()
+	converter.moveFileToOutput = function (callback) {
+		if ( ! converter.outputOptions.outputDir ) return callback()
 
-		var oldPath = self.getOutputFilePath()
-		var newPath = self.getFinalOutputFilePath()
+		var oldPath = converter.getOutputFilePath()
+		var newPath = converter.getFinalOutputFilePath()
 
-		self.outputFilePath = newPath
+		converter.outputFilePath = newPath
 	
 		fs.rename(oldPath, newPath, callback)
 	}
 
-	self.getConvertCommand = function () {
+	converter.getConvertCommand = function () {
 		var command = 'unoconv -f %s %s' 
-		return util.format(command, self.outputOptions.outputFormat, self.filePath)
+		return util.format(command, converter.outputOptions.outputFormat, converter.filePath)
 	}
-	
-	return self
+
+	return converter
 }
 
-module.exports = officeConverter
+module.exports = {
+	'converter': converter,
+	'listener': listener
+}
--- test/convert-test.js
+++ test/convert-test.js
@@ -1,7 +1,8 @@
 'use strict'
 var async = require('async')
 var test = require('tape')
-var officeConverter = require('../')
+var officeConverter = require('../').converter
+var listenerFunc = require('../').listener
 var path = require('path')
 var util = require('util')
 var inputFormats =  ['ppt', 'pptx', 'doc', 'docx']
@@ -13,15 +14,17 @@ test('Should be create officeConverter properly', function (t) {
 	t.ok(officeConverter, 'officeConverter should be exist')
 	t.equals(typeof officeConverter, 'function', 'officeConverter should be a function')
 
+	t.ok(listenerFunc, 'listenerFunc should be exist')
+	t.equals(typeof listenerFunc, 'function', 'listenerFunc should be a function')
+
 	var filePath = path.join(__dirname, 'test-files', 'test.ppt')
-	var allowQueue = false
 	var timeout = 550
 	var options = {
-		allowQueue: allowQueue,
 		timeout: timeout
 	}
 
 	var converter = officeConverter(filePath, options)
+	var listener = listenerFunc()
 	
 	t.ok(converter, 'converter should be exist')		
 	t.ok(converter instanceof {}.constructor, 'converter should be a json object')
@@ -29,12 +32,22 @@ test('Should be create officeConverter properly', function (t) {
 	t.ok(converter.options, 'converter.options should be exist')		
 	t.ok(converter.options instanceof {}.constructor, 'converter.options should be a json object')
 
-	t.equals(converter.options.allowQueue, allowQueue, 'converter.options.allowQueue should be equal to allowQueue')
-	
+	t.ok(listener.options, 'listener.options should be exist')		
+	t.ok(listener.options instanceof {}.constructor, 'listener.options should be a json object')
+
 	t.equals(converter.options.timeout, timeout,'converter.options.timeout should be equal to timeout')
 	
 	t.ok(converter.convert, 'converter.convert should be exist')
 	t.equals(typeof converter.convert, 'function', 'converter.convert should be a function')
+
+	t.ok(listener._getProcessCommand, 'listener._getProcessCommand should be exist')
+	t.equals(typeof listener._getProcessCommand, 'function', 'listener._getProcessCommand should be a function')
+
+	t.ok(listener._getListenerCommand, 'listener._getListenerCommand should be exist')
+	t.equals(typeof listener._getListenerCommand, 'function', 'listener._getListenerCommand should be a function')
+
+	t.ok(listener._getSofficeCommand, 'listener._getSofficeCommand should be exist')
+	t.equals(typeof listener._getSofficeCommand, 'function', 'listener._getSofficeCommand should be a function')
 
 	t.ok(converter.getConvertCommand, 'converter.getConvertCommand should be exist')
 	t.equals(typeof converter.getConvertCommand, 'function', 'converter.getConvertCommand should be a function')	
@@ -69,15 +82,34 @@ test('Should be create officeConverter properly', function (t) {
 	t.ok(converter.filePath, 'converter.filePath should be exist')
 	t.equals(converter.filePath, filePath, 'converter.filePath should be a function')
 
-	t.ok(converter.appendToQueue, 'converter.appendToQueue should be exist')
-	t.equals(typeof converter.appendToQueue, 'function', 'converter.appendToQueue should be a function')
+	t.ok(listener.exists, 'listener.exists should be exist')
+	t.equals(typeof listener.exists, 'function', 'listener.exists should be a function')
+
+	t.ok(listener.create, 'listener.create should be exist')
+	t.equals(typeof listener.create, 'function', 'listener.create should be a function')
 	
-	t.ok(converter.existsPreviousProcess, 'converter.existsPreviousProcess should be exist')
-	t.equals(typeof converter.existsPreviousProcess, 'function', 'converter.existsPreviousProcess should be a function')
+	t.ok(listener.kill, 'listener.kill should be exist')
+	t.equals(typeof listener.kill, 'function', 'listener.kill should be a function')
+	
+	t.ok(listener.pid, 'listener.pid should be exist')
+	t.equals(typeof listener.pid, 'function', 'listener.pid should be a function')
+	
+	function existsListener (cb) {
+		function createListener (callback) {
+			listener.create(callback)	
+		}
 
-	var epp = converter.existsPreviousProcess()
-	t.ok(epp === false || epp === true, 'converter.existsPreviousProcess should be equal to true or false')
+		listener.exists((err, exists) => {
+			t.error(err, 'should not be an error in listener.exists')
+			t.ok(exists === false || exists === true, 'listener.exists should be equal to true or false')
 
+			if ( exists === false ) createListener(cb)
+
+			cb()
+		})
+	}
+
+	
 	function inputFileExists (cb) {
 		converter.inputFileExists(err => {
 			t.error(err, 'should not be an error in inputFileExists')
@@ -97,6 +129,7 @@ test('Should be create officeConverter properly', function (t) {
 	}
 	
 	async.series([
+		existsListener,
 		inputFileExists,
 		outputFileExists
 	], err => {
@@ -104,6 +137,94 @@ test('Should be create officeConverter properly', function (t) {
 
 		t.end()
 	})
+})
+
+test('should be throw exception', function (t) {
+	var filePath = path.join(__dirname, 'test-files', 'test.ppt')
+	var outputFormat = 'pdf'
+	var outputDir = path.join(__dirname, 'files-output')
+	var except
+
+	try {	
+		
+		var converter = officeConverter(filePath)
+		converter.convert((err, filePath) => {})
+	} catch (e) {
+		except = e
+	}
+
+	t.ok(except, 'should be throw exception if there are not options.')	
+	except = undefined
+
+	try {	
+		var converter = officeConverter()
+	} catch (e) {
+		except = e
+	}
+
+	t.ok(except, 'should be throw exception if there is not filePath.')	
+	except = undefined
+
+	t.end()
+	
+})
+
+test('Fail creating listener', function (t) {
+	var listener = listenerFunc()
+	
+	listener.kill((err) => {
+		t.error(err, 'should not be an error killing listener')
+		listener.create((err) => {
+			t.ok(err, 'should be and error creating listener')
+			t.end()
+		})
+
+		setTimeout(() => listener.kill(function () {}), Math.round(listener.options.createTimeout/2))
+	})
+})
+
+test('Prev convert kill listener', function (t) {
+	var listener = listenerFunc()
+	
+	function existsListener (cb) {
+		function createListener (callback) {
+			listener.create(callback)	
+		}
+
+		listener.exists((err, exists) => {
+			t.error(err, 'should not be an error in listener.exists')
+			t.ok(exists === false || exists === true, 'listener.exists should be equal to true or false')
+
+			if ( exists === false ) return createListener(cb)
+
+			cb()
+		})
+	}
+
+	function killListener (cb) {
+		listener.kill(cb)	
+	}
+
+	async.series([
+		existsListener,
+		killListener
+	], err => {
+		t.error(err, 'should not be an error in async.series')
+
+		t.end()
+	})
+})
+
+test('Kill listener fails', function (t) {
+	var listener = listenerFunc()	
+	var killListener = err => {
+		listener.kill(err => {
+			t.ok(err, 'should be an error in kill cb')
+			t.end()
+		})	
+	}
+
+	listener.kill(killListener)	
 })
 
 test('should convert files', function (t) {		
@@ -177,29 +298,6 @@ test('should convert files', function (t) {
 	whileConverting()
 })
 
-test('should be throw exception', function (t) {
-	var filePath = path.join(__dirname, 'test-files', 'test.ppt')
-	var outputFormat = 'pdf'
-	var outputDir = path.join(__dirname, 'files-output')
-
-	try {	
-		
-		var converter = officeConverter(filePath)
-		converter.convert((err, filePath) => {})
-	} catch (e) {
-		t.ok(e, 'should be throw exception if there are not options.')	
-	}
-
-	try {	
-		var converter = officeConverter()
-	} catch (e) {
-		t.ok(e, 'should be throw exception if there is not filePath.')	
-	}
-
-	t.end()
-	
-})
-
 test('should be fail converting files', function (t) {		
 	var i_length = inputFormats.length
 	var o_length = outputFormats.length
@@ -247,6 +345,38 @@ test('should be fail converting files', function (t) {
 		failByFilePath,
 		failByOutputExt		
 	], err => {
+		t.end()
+	})
+})
+
+test('After convert kill listener', function (t) {
+	var listener = listenerFunc()
+	
+	function existsListener (cb) {
+		function createListener (callback) {
+			listener.create(callback)	
+		}
+
+		listener.exists((err, exists) => {
+			t.error(err, 'should not be an error in listener.exists')
+			t.ok(exists === false || exists === true, 'listener.exists should be equal to true or false')
+
+			if ( exists === false ) return createListener(cb)
+
+			cb()
+		})
+	}
+
+	function killListener (cb) {
+		listener.kill(cb)	
+	}
+
+	async.series([
+		existsListener,
+		killListener
+	], err => {
+		t.error(err, 'should not be an error in async.series')
+
 		t.end()
 	})
 })
